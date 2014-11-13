43d4bbfc8a4e429a40ec558abe80e88c5b6758d2
kangkyu <kangkyu1111@gmail.com>
2014-11-13 15:10:33 -0800

    add controller and view of task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    # @task.save
+    redirect_to root_url
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :comment, :done_at)
+  end
 end
