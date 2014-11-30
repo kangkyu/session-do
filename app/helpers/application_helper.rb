0d0ef1a935cc54f56c2f76b7e8d184ab29d69c5b
kangkyu <kangkyu1111@gmail.com>
2014-11-29 21:33:47 -0800

    make current_user only query once per redirect

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
