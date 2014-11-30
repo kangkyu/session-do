a010604a872f331ae4523787903c3a1bae089ec5
kangkyu <kangkyu1111@gmail.com>
2014-11-29 20:51:46 -0800

    add users new (register) controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.password_confirmation = @user.password
+    if @user.save
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   get 'tasks/:id/refresh', to: 'tasks#refresh', as: 'refresh_task'
 
-  get 'users/:id', to: 'users#show', as: 'user'
+  resources :users
   get 'register', to: 'users#new'
+
+  resource :session
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
