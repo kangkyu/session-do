fe9a34d62be84bfdb14e71b25689f541ac5649ca
webkay <websitekay@gmail.com>
2014-12-02 03:14:45 -0800

    get user password_confirmation attribute a better way

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.password_confirmation = @user.password
+    @user.password_confirmation = user_params[:password]
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_url
