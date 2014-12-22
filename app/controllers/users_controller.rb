a3f58ce557ee6faa10a2ec38b5967690e7a8db41
webkay <websitekay@gmail.com>
2014-12-22 19:05:25 +0000

    add users_controller edit method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
