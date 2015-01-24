cf7db87bae4631a0c2b58f64644817b9614c4c34
kangkyu <kangkyu1111@gmail.com>
2015-01-24 12:19:16 -0800

    apply different color and change page titles

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,14 +1,29 @@
 module TasksHelper
 
-  def daily_badge(task)
-    'daily' if task.is_daily
+  def task_name(task)
+    if task.is_daily
+      daily_task_name(task)
+    else
+      non_daily_task_name(task)
+    end
   end
 
   def link_to_daily(task, options={})
     if task.is_daily
-      link_to icon('arrow-right'), toggle_daily_task_path(task), class:'btn btn-info'
+      link_to icon('arrow-right'), toggle_daily_task_path(task), class:'btn btn-primary'
     else
       link_to icon('arrow-left'), toggle_daily_task_path(task), class:'btn btn-success'
     end
   end
+
+  private
+
+  def daily_task_name(task)
+    content_tag(:span, task.name, class: ["btn", "btn-daily"])
+  end
+
+  def non_daily_task_name(task)
+    content_tag(:span, task.name, class: ["btn", "btn-non-daily"])
+  end
+  
 end
