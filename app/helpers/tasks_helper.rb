17114b0ffb225acd74241344c49e2b20a06c7dff
kangkyu <kangkyu1111@gmail.com>
2015-02-02 14:53:26 -0800

    fix and center bar text for future task

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -24,21 +24,23 @@ module TasksHelper
   def progress_bar_tag(task)
 
     if task.time_passed_by < 0
-      content_tag(:div, 
+      content_tag(:div, "",
         "aria-valuemax" => "100", "aria-valuemin" => "0", "aria-valuenow" => task.bar_length,
-        :role => "progressbar", :style => "width: #{task.bar_length}%; display: block; float: right;",
-        class: ["progress-bar", "progress-bar-warning"]) do
-          content_tag(:span,
-            distance_of_time_in_words_to_now(task.done_at).concat(" later"))
-      end
+        :role => "progressbar", :style => "width: #{task.bar_length}%; float: right;",
+        class: ["progress-bar", "progress-bar-warning"])
     else
       content_tag(:div,
         "aria-valuemax" => "100", "aria-valuemin" => "0", "aria-valuenow" => task.bar_length,
         :role => "progressbar", :style => "width: #{task.bar_length}%;",
         class: ["progress-bar", progress_bar_class(task)]) do
-          content_tag(:span,
-            distance_of_time_in_words_to_now(task.done_at).concat(" ago"))
+          content_tag(:span, distance_of_time_in_words_to_now(task.done_at).concat(" ago"))
       end
+    end
+  end
+
+  def progress_bar_text(task)
+    if task.time_passed_by < 0
+      content_tag(:span, distance_of_time_in_words_to_now(task.done_at).concat(" later"), class: "bar-text-color-white")
     end
   end
 
