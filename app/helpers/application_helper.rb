8adfd7d859715209178c442d7dda51909aedddd8
kangkyu <kangkyu1111@gmail.com>
2015-03-03 11:17:28 -0800

    change title 'tasks -> tasks in touch'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def class_name_by_key(key)
     case key 
-    when "notice" then "alert-info"
+    when "notice" then "alert-success"
     when "alert" then "alert-warning" 
     else "alert-default"
     end
