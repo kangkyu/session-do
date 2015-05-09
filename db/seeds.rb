7e7033ea05ac3d7123f83a36bd7ed949013ab2ea
kangkyu <kangkyu1111@gmail.com>
2015-05-09 12:38:50 -0700

    move task form button to the right side, edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,14 @@
 
 User.delete_all
 Task.delete_all
-User.create!(username: "jimmy", email: "jimmy@example.com", password: "1111")
-Task.create!(name: 'training', comment: 'work hard!', done_at: Time.now.in_time_zone - 2.days, user: User.first)
+users = User.create!([
+  {username: "jimmy", email: "jimmy@example.com", password: "1111"},
+  {username: "kang", email: "kang@example.com", password: "1111"}
+  ])
+task = Task.create!(name: 'training', comment: 'work hard!', done_at: Time.now.in_time_zone - 2.days, user: User.first)
+
+visits = Visit.create!([
+  {task: task, note: "But I must explain to you how all this mistaken idea of denouncing of a pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. 
+    Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"},
+  {task: task, note: "visited!"}
+])
