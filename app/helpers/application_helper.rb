bc3d4d0f01038931ae5778dfa536232de00ab47f
kangkyu <kangkyu1111@gmail.com>
2015-08-14 15:43:25 -0700

    edit tab_bar partial with template helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,34 @@
 module ApplicationHelper
 
   def class_name_by_key(key)
-    case key 
+    case key
     when "notice" then "alert-success"
-    when "alert" then "alert-warning" 
+    when "alert" then "alert-warning"
     else "alert-default"
     end
   end
 
+  def helper_bar_center_button
+    if controller_name == 'sessions' && action_name == 'new' # :session_new
+      link_to "Sign Up", register_path, class: 'btn btn-link'
+    elsif controller_name == 'users' && action_name == 'new' # :user_new
+      link_to "Log In", login_path, class: 'btn btn-link'
+    elsif controller_name == 'tasks' && action_name == 'index' # :task_index
+      link_to fa_icon_with_removed_text('plus', text: "Add Task", right: false), new_task_path, class: 'btn btn-non-daily'
+    elsif (controller_name == 'tasks' && action_name == 'edit') ||
+      (controller_name == 'tasks' && action_name == 'new') ||
+      (controller_name == 'users' && action_name == 'edit') ||
+      (controller_name == 'tasks' && action_name == 'show')
+    # :task_form, :user_edit, :task_show
+      link_to fa_icon('arrow-left', text: 'Back', right: false), tasks_path, class: 'btn btn-non-daily'
+    else
+      "Any link or button?"
+    end
+  end
+
+  def helper_bar_right_button
+    if controller_name == 'users' && action_name == 'new'
+      link_to "First time?", welcome_guide_path, class: 'btn btn-link'
+    end
+  end
 end
