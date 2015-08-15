ca588bb9ff3c649f3d8615b80e87a5cbea67602c
kangkyu <kangkyu1111@gmail.com>
2015-08-14 20:40:44 -0700

    define and use different helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,26 +8,7 @@ module ApplicationHelper
     end
   end
 
-  def helper_bar_center_button
-    if controller_name == 'sessions' && action_name == 'new' # :session_new
-      link_to "Sign Up", register_path, class: 'btn btn-link'
-    elsif controller_name == 'users' && action_name == 'new' # :user_new
-      link_to "Log In", login_path, class: 'btn btn-link'
-    elsif controller_name == 'tasks' && action_name == 'index' # :task_index
-      link_to fa_icon_with_removed_text('plus', text: "Add Task", right: false), new_task_path, class: 'btn btn-non-daily'
-    elsif (controller_name == 'tasks' && action_name == 'edit') ||
-      (controller_name == 'tasks' && action_name == 'new') ||
-      (controller_name == 'users' && action_name == 'edit')
-    # :task_form, :user_edit
-      link_to fa_icon('arrow-left', text: 'Back', right: false), tasks_path, class: 'btn btn-non-daily'
-    else
-      "Any link or button?"
-    end
-  end
-
-  def helper_bar_right_button
-    if controller_name == 'users' && action_name == 'new'
-      link_to "First time?", welcome_guide_path, class: 'btn btn-link'
-    end
+  def current_cont?(controller, action)
+    controller_name == controller && action_name == action
   end
 end
