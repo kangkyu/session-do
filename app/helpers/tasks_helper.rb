da90b97fa4d5ba1c1fafa1810185b634271519fe
kangkyu <kangkyu1111@gmail.com>
2015-09-20 23:37:17 -0700

    make the bar green for dailies
    keep the non-daily bars & text blue (blue for non-daily, green for dailies)
    edit the helper methods progress_bar_tag

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,9 @@
 module TasksHelper
 
   def task_name(task)
-    content_tag(:span, truncate(task.name), class: [(task.is_daily ? "task-daily" : "task-non-daily")], id: 'task-name')
+    task_name_class = task.is_daily ? "task-daily" : "task-non-daily"
+
+    content_tag(:span, truncate(task.name), class: task_name_class, id: 'task-name')
   end
 
   def fa_icon_with_removed_text(icon_name, options={})
@@ -11,6 +13,7 @@ module TasksHelper
   end
 
   def progress_bar_tag(task)
+    bar_color_class = task.is_daily ? "progress-bar-success" : "progress-bar-primary"
 
     if task.time_passed_by < 0
       content_tag(:div, "",
@@ -21,7 +24,7 @@ module TasksHelper
       content_tag(:div,
         "aria-valuemax" => "100", "aria-valuemin" => "0", "aria-valuenow" => task.bar_length,
         :role => "progressbar", :style => "width: #{task.bar_length}%;",
-        class: ["progress-bar", "progress-bar-primary"]) do
+        class: ["progress-bar", bar_color_class]) do
           content_tag(:span, distance_of_time_in_words_to_now(task.done_at).concat(" ago"))
       end
     end
