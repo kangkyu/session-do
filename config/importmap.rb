3f0acd5b4503059f35e85927e55c8c5a72c4a863
kangkyu <kangkyu1111@gmail.com>
2022-02-26 21:55:22 -0800

    Add bootstrap-datepicker and some style change

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,7 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.2/lib/index.js"
+pin "bootstrap-datepicker", to: "https://ga.jspm.io/npm:bootstrap-datepicker@1.9.0/dist/js/bootstrap-datepicker.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js", preload: true
