dd6272ba6c694dea8c08abc12871c38f92ef5db9
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-04-13 19:36:42 -0700

    Fix: css_compressor nil

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,5 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+Rails.application.config.assets.css_compressor = nil
