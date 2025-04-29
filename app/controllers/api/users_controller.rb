79016c38b1952aa673487dc12e99f9baa758673f
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-04-28 20:22:31 -0700

    User signup in API

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  class UsersController < Api::BaseController
+
+    def create
+      @user = User.new(user_params)
+      @user.password_confirmation = user_params[:password]
+      if @user.save
+        render json: { user_id: @user.id }, status: :created
+      else
+        render json: { error: 'User not registered' }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+  end
+end
